c0a32113ceb5889d3f100f281c76b351617748da
Thet Wai Mon <twmon@192.168.1.2>
2023-07-09 15:55:38 +0630

    modified some versions in pom and anyrequests authenticated tested

---
+++ src/main/java/co/jp/hello/config/SpringSecConfig.java
@@ -0,0 +1,22 @@
+package co.jp.hello.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecConfig {
+
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .anyRequest().authenticated()
+            )
+            .httpBasic();
+        return http.build();
+    }
+
+
+}
--- src/main/java/co/jp/hello/controller/SpringSecController.java
+++ src/main/java/co/jp/hello/controller/SpringSecController.java
@@ -12,16 +12,33 @@ import co.jp.hello.dto.UserDto;
 import co.jp.hello.service.SpringSecService;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
 public class SpringSecController {
 	@Autowired
 	SpringSecService springService;
 	
-	@GetMapping("/list")
+	@GetMapping("list")
 	public List<UserDto> getUsers(){
 		return this.springService.getUsers();
 	}
+	
+	@GetMapping("account")
+	public String account(){
+		return "this is account.";
+	}
+	
+	@GetMapping("balance")
+	public String balance(){
+		return "your balance is ...";
+	}
+	
+	@GetMapping("update")
+	public String update(){
+		return "it is updated.";
+	}
+	
+	
 }
 
 
